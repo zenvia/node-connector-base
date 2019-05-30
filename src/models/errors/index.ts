eb30138d0a72689b0acb3c8b6c6d3c9cbc62bc1b
Rodrigo Kamada <rodrigo@kamada.com.br>
2019-05-30 18:38:17 -0300

    Polish.

--- src/models/errors/index.ts
+++ src/models/errors/index.ts
@@ -1,5 +1,5 @@
 export { ApplicationError } from './abstract-application-error';
+export { ClientError } from './client-error';
 export { IErrorDTO } from './error-dto';
 export { IErrorDetail } from './error-detail';
-export { ClientError } from './client-error';
 export { ValidationError } from './validation-error';
