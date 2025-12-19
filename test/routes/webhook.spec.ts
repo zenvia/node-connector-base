c4dba8a64db655bc644eacdb1c22287624a574d2
felipedalcarobo <felipedalcarobo@ymail.com>
2025-12-19 10:21:44 -0300

    test fix

--- test/routes/webhook.spec.ts
+++ test/routes/webhook.spec.ts
@@ -3,13 +3,12 @@ import * as request from 'supertest';
 import * as sinon from 'sinon';
 import * as app from '../../src/app';
 import * as webhookHandler from '../../src/handlers/webhook-handler';
-import { IWebhook } from '../../src/models/webhook';
 import { App } from 'supertest/types';
 
 describe('/webhook endpoint', () => {
 
   let appInit: App;
-  let webhookHandlerStub: sinon.SinonStub<[webhook: IWebhook], Promise<void>>;
+  let webhookHandlerStub: sinon.SinonStub<[webhook: any], Promise<void>>;
 
   before(async () => {
     appInit = await app.init();
@@ -28,8 +27,11 @@ describe('/webhook endpoint', () => {
         message: 'SOME_MESSAGE',
       };
 
+      const token = 'WEBHOOK_TOKEN';
+
       request(appInit)
       .post('/v1/webhook')
+      .set('x-auth-token', token)
       .send(payload)
       .expect(204)
       .end((err, res) => {
