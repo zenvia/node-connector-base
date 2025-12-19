5f0ef4d1e47680e62f3bbaf1d8821682a2f01858
felipedalcarobo <felipedalcarobo@ymail.com>
2025-12-19 09:37:21 -0300

    small fix

--- src/models/errors/error.d.ts
+++ src/models/errors/error.d.ts
@@ -1,10 +1,8 @@
 import { IErrorDetail } from './error-detail';
 
 export interface IError {
-
   code: string;
   message: string;
   httpStatusCode: number;
   details?: IErrorDetail[];
-
 }
--- src/zenvia-custom-service/message-handler-service.ts
+++ src/zenvia-custom-service/message-handler-service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import logger from "@zenvia/logger";
 import * as config from 'config';
-import { IMessage, IZenviaConfig } from './message';
+import { IMessage, IMessageStatus, IZenviaConfig } from './message';
 
 const zenviaConfig: IZenviaConfig = config.get('zenvia');
 const uri = zenviaConfig.uri;
@@ -30,13 +30,13 @@ export async function send(message: IMessage, token: string): Promise<any> {
   }
 }
 
-export async function sendMessageStatus(message: IMessage, token: string): Promise<any> {
-  logger.debug('Sending the message status to Zenvia: ', { message });
+export async function sendMessageStatus(messageStatus: IMessageStatus, token: string): Promise<any> {
+  logger.debug('Sending the message status status to Zenvia: ', { messageStatus });
 
   try {
-    logger.info('Sending the status request to uri', uri);
+    logger.info('Sending the message status request to uri', uri);
 
-    const response = await axios.post(uri, message, {
+    const response = await axios.post(uri, messageStatus, {
       headers: {
         'X-AUTH-TOKEN': token,
         'Content-Type': 'application/json',
@@ -45,7 +45,7 @@ export async function sendMessageStatus(message: IMessage, token: string): Promi
       responseType: 'json',
     });
 
-    logger.debug('Status sent successfully. Response from Zenvia: ', { uri, response });
+    logger.debug('Message status sent successfully. Response from Zenvia: ', { uri, response });
 
     return response.data;
   } catch (error: any) {
